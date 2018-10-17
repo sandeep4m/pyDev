d75411d4b64c0d8b1d49abc4a74fe819fcb26323
sandeep <sandeep41195@gmail.com>
2018-10-17 15:42:58 +0530

    star player attribute added

--- dream11NBA/fantasy.py
+++ dream11NBA/fantasy.py
@@ -7,9 +7,16 @@ from player import Player
 
 class Fantasy(Stats, Player):
 
-    def __init__(self, profile, stats):
+    def __init__(self, profile, stats, star=False):
         Stats.__init__(self, stats)
         Player.__init__(self, profile)
+        self.isStarPlayer = star
+
+    def __str__(self):
+        output = (self.name + " fantasy stats".upper(),
+                  "Points: " + str(self.fantasyPoints()),
+                  "Index: " + str(self.fantasyIndex()))
+        return("\n".join(output))
 
     def fantasyPoints(self):
         return(self.PPG + 1.2 * self.RPG + 1.5 * self.APG
@@ -18,11 +25,10 @@ class Fantasy(Stats, Player):
     def fantasyIndex(self):
         return(self.fantasyPoints() / self.credits)
 
-    def __str__(self):
-        output = (self.name + " fantasy stats".upper(),
-                  "Points: " + str(self.fantasyPoints()),
-                  "Index: " + str(self.fantasyIndex()))
-        return("\n".join(output))
+    def pointsGot(self):
+        if self.isStarPlayer:
+            return(2 * self.fantasyPoints())
+        return(self.fantasyPoints())
 
 
 if __name__ == "__main__":
